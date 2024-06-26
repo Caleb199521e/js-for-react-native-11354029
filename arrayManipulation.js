307d38ba2afe00f8c6e96c5c9621c5c670654d0c
CALEB AMPONG <campong004@st.ug.edu.gh>
2024-06-26 20:53:04 +0000

    added createUserProfiles function

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,17 +1,14 @@
-function processArray(arr) {
-    return arr.map(num => num % 2 === 0 ? num ** 2 : num * 3);
+// Task 1: processArray function
+function processArray(numbers) {
+    return numbers.map(num => num % 2 === 0 ? num * num : num * 3);
 }
 
-function formatArrayStrings(stringArray, numberArray) {
-    return stringArray.map((str, index) => {
-        const num = numberArray[index];
+// Task 2: formatArrayStrings function
+function formatArrayStrings(strings, processedNumbers) {
+    return strings.map((str, index) => {
+        const num = processedNumbers[index];
         return num % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
     });
 }
 
-// Example usage:
-const inputStrings = ["Hello", "World", "JavaScript", "Is", "Fun"];
-const inputNumbers = [1, 2, 3, 4, 5];
-const processedNumbers = processArray(inputNumbers);
-const formattedStrings = formatArrayStrings(inputStrings, processedNumbers);
-console.log(formattedStrings); // Output: ["hello", "WORLD", "javascript", "IS", "fun"]
+module.exports = { processArray, formatArrayStrings };
