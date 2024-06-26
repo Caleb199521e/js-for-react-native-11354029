968535e9a4ce47bf59029ff07abc33d17acfd7a7
CALEB AMPONG <campong004@st.ug.edu.gh>
2024-06-26 20:19:07 +0000

    added formatArrayStrings function

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -2,7 +2,16 @@ function processArray(arr) {
     return arr.map(num => num % 2 === 0 ? num ** 2 : num * 3);
 }
 
+function formatArrayStrings(stringArray, numberArray) {
+    return stringArray.map((str, index) => {
+        const num = numberArray[index];
+        return num % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+    });
+}
 
-const inputArray = [1, 2, 3, 4, 5];
-const resultArray = processArray(inputArray);
-console.log(resultArray); 
+// Example usage:
+const inputStrings = ["Hello", "World", "JavaScript", "Is", "Fun"];
+const inputNumbers = [1, 2, 3, 4, 5];
+const processedNumbers = processArray(inputNumbers);
+const formattedStrings = formatArrayStrings(inputStrings, processedNumbers);
+console.log(formattedStrings); // Output: ["hello", "WORLD", "javascript", "IS", "fun"]
